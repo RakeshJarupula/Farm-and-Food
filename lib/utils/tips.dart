c69691ed3620306f9fd5cdda02ed0660b9ca9ceb
Rakesh <jarupularakesh1482@gmail.com>
2024-07-25 20:00:58 +0530

    Add Random Tips Using Gemini

---
+++ lib/utils/tips.dart
@@ -0,0 +1,14 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:farm_and_food/api/gemini_service.dart';
+
+Future<String?> getHealthTip() async {
+  const String textPartFood = '''Generate food health tip in just one sentence. Be specific. Nothing else. Start the output with "Tip:" ''';
+  final response = await geminiModel.generateContent([Content.text(textPartFood)]);
+  return response.text;
+}
+
+Future<String?> getFarmTip() async {
+  const String textPartFarm = '''Generate farm tip in just one sentence. Be specific. Nothing else.. Start the output with "Tip:" ''';
+  final response = await geminiModel.generateContent([Content.text(textPartFarm)]);
+  return response.text;
+}
