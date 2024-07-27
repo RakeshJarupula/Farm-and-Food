2f403fe72f3d3ffb1057a180269a0bda8166c167
Rakesh <jarupularakesh1482@gmail.com>
2024-07-27 16:29:19 +0530

    Change Logo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -97,7 +97,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 30),
+                  const SizedBox(height: 50),
                   GestureDetector(
                     onTap: () {
                       Navigator.push(
